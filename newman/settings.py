39cc4c261137f6eca813cb1e312bebc80d311191
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2025-04-29 13:27:40 +0000

    added domian name to allowed host

--- newman/settings.py
+++ newman/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.newmanstudio.art', '137.184.59.130', 'localhost', '127.0.0.1']
 
 
 # Application definition
